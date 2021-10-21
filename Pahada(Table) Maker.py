a7da242585d33c2bf1d54fa37cea29ce8b3fa592
Galactic-Boss-69 <85726780+GauravRawat7@users.noreply.github.com>
2021-10-21 20:06:23 +0530

    Add files via upload

---
+++ Pahada(Table) Maker.py
@@ -0,0 +1,10 @@
+# PAHADA MAKER---------------------------------------
+def pahada(x,y = 10):
+   i = 0
+   for i in range(y):
+      f = (i + 1) * x
+      print(f)
+# You can use this program to write tables of any number to any extent 
+# x = Number
+# y = Range in which you want the table to print
+
